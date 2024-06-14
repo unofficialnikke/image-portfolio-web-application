5a69e882dd0c9b9fb4a4dfec1508e6e6199e6860
unofficialnikke <nicklasnystrm3@gmail.com>
2024-06-14 12:07:49 +0300

    feat: create home page

---
+++ frontend/src/mockdata/data.ts
@@ -0,0 +1,118 @@
+import Image1 from '../img/1.jpg'
+import Image2 from '../img/2.jpg'
+import Image3 from '../img/3.jpg'
+
+export const profiles = [
+    {
+        id: 1,
+        firstname: 'John',
+        lastname: 'Cena',
+        city: 'Helsinki',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'nature', 'portrait', 'sports'
+        ],
+    },
+    {
+        id: 2,
+        firstname: 'Jane',
+        lastname: 'Doe',
+        city: 'New York',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'architecture', 'fashion', 'travel'
+        ],
+    },
+    {
+        id: 3,
+        firstname: 'Michael',
+        lastname: 'Smith',
+        city: 'London',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'street', 'event', 'food'
+        ],
+    },
+    {
+        id: 4,
+        firstname: 'Emily',
+        lastname: 'Johnson',
+        city: 'Los Angeles',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'animals', 'black and white', 'abstract'
+        ],
+    },
+    {
+        id: 5,
+        firstname: 'John',
+        lastname: 'Cena',
+        city: 'Helsinki',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'nature', 'portrait', 'sports'
+        ],
+    },
+    {
+        id: 6,
+        firstname: 'Jane',
+        lastname: 'Doe',
+        city: 'New York',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'architecture', 'fashion', 'travel'
+        ],
+    },
+    {
+        id: 7,
+        firstname: 'Michael',
+        lastname: 'Smith',
+        city: 'London',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'street', 'event', 'food'
+        ],
+    },
+    {
+        id: 8,
+        firstname: 'Emily',
+        lastname: 'Johnson',
+        city: 'Los Angeles',
+        images: [
+            { src: Image1 },
+            { src: Image2 },
+            { src: Image3 }
+        ],
+        categories: [
+            'animals', 'black and white', 'abstract'
+        ],
+    },
+]
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { profiles } from '../mockdata/data'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <div className="profiles">
+                {profiles.map(profile => (
+                    <div className='profile' key={profile.id}>
+                        <div className='header'>
+                            <Link className='link' to={`/profile/${profile.id}`}><h2>{profile.firstname} {profile.lastname}</h2></Link>
+                            <h4>{profile.city}</h4>
+                        </div>
+                        <div className='category'>
+                            {profile.categories.map(category => (
+                                <p>{category},</p>
+                            ))}
+                        </div>
+                        <div className='image'>
+                            {profile.images.map(image => (
+                                <img src={image.src} />
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
