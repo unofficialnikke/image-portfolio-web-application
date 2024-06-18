e9afdf004749028444c9d0805d87ec63cb807923
unofficialnikke <nicklasnystrm3@gmail.com>
2024-06-19 01:23:13 +0300

    feat: about page styling

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='about'>About</div>
+        <div className='about'>
+            <div className="content">
+                <div className="headercontent">
+                    <form>
+                        <h2>Welcome to the portfolio platform!</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis vel obcaecati libero in! Qui expedita autem amet tempora iusto voluptatum. Repellat eum sapiente sint, porro temporibus voluptate adipisci recusandae molestias.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis vel obcaecati libero in! Qui expedita autem amet tempora iusto voluptatum. Repellat eum sapiente sint, porro temporibus voluptate adipisci recusandae molestias.</p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis vel obcaecati libero in! Qui expedita autem amet tempora iusto voluptatum. Repellat eum sapiente sint, porro temporibus voluptate adipisci recusandae molestias.
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis vel obcaecati libero in! Qui expedita autem amet tempora iusto voluptatum. Repellat eum sapiente sint, porro temporibus voluptate adipisci recusandae molestias.</p>
+                    </form>
+                </div>
+                <div className="footercontent">
+                    <form>
+                        <h2>Frequently asked questions:</h2>
+                        <h3>Question 1</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <h3>Question 2</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit illo iusto totam molestiae. Voluptas modi amet esse culpa iste facere, adipisci dignissimos asperiores, libero odit aspernatur, ratione incidunt at mollitia. </p>
+                        <h3>Question 3</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
