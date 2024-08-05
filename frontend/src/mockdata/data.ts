d0252a090677f88d4e91b73fe7b80d36e2ef05c4
unofficialnikke <nicklasnystrm3@gmail.com>
2024-08-06 01:43:34 +0300

    revert: mockdata changes

---
+++ frontend/src/mockdata/data.ts
@@ -0,0 +1,147 @@
+import Image1 from '../img/1.jpg'
+import Image2 from '../img/2.jpg'
+import Image3 from '../img/3.jpg'
+
+export const images = [
+    {
+        id: 1,
+        src: Image1
+    },
+    {
+        id: 2,
+        src: Image2
+    }, {
+        id: 3,
+        src: Image3
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        name: 'nature'
+    },
+    {
+        id: 2,
+        name: 'portrait'
+    },
+    {
+        id: 3,
+        name: 'sports'
+    }
+]
+
+export const profiles = [
+    {
+        id: 1,
+        firstname: 'John',
+        lastname: 'Cena',
+        city: 'Helsinki',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'nature', 'portrait', 'sports'
+        ],
+    },
+    {
+        id: 2,
+        firstname: 'Jane',
+        lastname: 'Doe',
+        city: 'New York',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'architecture', 'fashion', 'travel'
+        ],
+    },
+    {
+        id: 3,
+        firstname: 'Michael',
+        lastname: 'Smith',
+        city: 'London',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'street', 'event', 'food'
+        ],
+    },
+    {
+        id: 4,
+        firstname: 'Emily',
+        lastname: 'Johnson',
+        city: 'Los Angeles',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'animals', 'black and white', 'abstract'
+        ],
+    },
+    {
+        id: 5,
+        firstname: 'John',
+        lastname: 'Cena',
+        city: 'Helsinki',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'nature', 'portrait', 'sports'
+        ],
+    },
+    {
+        id: 6,
+        firstname: 'Jane',
+        lastname: 'Doe',
+        city: 'New York',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'architecture', 'fashion', 'travel'
+        ],
+    },
+    {
+        id: 7,
+        firstname: 'Michael',
+        lastname: 'Smith',
+        city: 'London',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'street', 'event', 'food'
+        ],
+    },
+    {
+        id: 8,
+        firstname: 'Emily',
+        lastname: 'Johnson',
+        city: 'Los Angeles',
+        images: [
+            { id: 1, src: Image1 },
+            { id: 2, src: Image2 },
+            { id: 3, src: Image3 }
+        ],
+        categories: [
+            'animals', 'black and white', 'abstract'
+        ],
+    },
+]
