e9d8a39752f22e7034d48f466623f594134bb54c
unofficialnikke <nicklasnystrm3@gmail.com>
2024-10-01 14:23:57 +0300

    style: change button style and add mock data to about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -22,14 +22,19 @@ const About = () => {
                 <div className="footercontent">
                     <form>
                         <h2>Frequently asked questions:</h2>
-                        <h3>Question 1</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                        <h3>Question 2</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit illo iusto totam molestiae.
-                            Voluptas modi amet esse culpa iste facere, adipisci dignissimos asperiores, libero odit
-                            aspernatur, ratione incidunt at mollitia. </p>
-                        <h3>Question 3</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <h3>How can I add images?</h3>
+                        <p>You can add images to your profile by going to your profile.
+                            <br /> 1. Click your profile on top right corner
+                            <br />2. Add new images by clicking the button "Add images"
+                            <br />3. You can upload up to 10 images. You can also delete your previous images, if you will.</p>
+                        <h3>How to showcase my images on home page?</h3>
+                        <p>You can showcase your images on home page by adding your images to favorites. You can select up to three favorite images.</p>
+                        <h3>How can I edit my profile?</h3>
+                        <p>You can edit your profile from your profile page.
+                            <br />1. On the bottom of your profile page, click "Edit" to edit your introduction text or your categories and social medias
+                            <br />2. You can select up to five categories to describe your photographying skills.
+                            <br />3. You can add your Instagram profile link, LinkedIn profile link and a link of your choice (your own portfolio page for example).
+                        </p>
                     </form>
                 </div>
             </div>
